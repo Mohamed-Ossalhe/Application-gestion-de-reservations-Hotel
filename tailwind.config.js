a70c4da90c9d28fc0413e80a88f948c6a54414d8
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2022-12-21 19:23:51 +0100

    installed TailwindCss Framework and adjusted some configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["app/view/home/", "app/view/admin"],
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}

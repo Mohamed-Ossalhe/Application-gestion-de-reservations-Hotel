78f3357583105a550357d829b3061f0620602d1d
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-01-08 17:39:59 +0100

    fix search form issues + display search form data + fix book room page issues

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -13,7 +13,9 @@ class homeController extends Controller {
         $this->view->render();
     }
     public function bookRoom($id) {
-        $this->view('home/book-room');
+        $this->model("Room");
+        $roomDetails = $this->model->getDataRow($id);
+        $this->view('home/book-room', ["roomDetails" => $roomDetails]);
         $this->view->render();
     }
     public function log_in($error = "") {
@@ -45,40 +47,7 @@ class homeController extends Controller {
                 // echo 'hello world am using ajax roomtype: '. $data['room_type'] . ' + no suite type';
             }
             $rooms = $this->model->searchData($data);
-            // $result = json_encode($rooms);
-            foreach($rooms as $room) {
-                $bookBtn = '';
-                $suiteType = '';
-                if(!isset($_SESSION["logged"]) and $_SESSION["logged"] !== true) {
-                    $bookBtn = "../home/log_in";
-                }else {
-                    $bookBtn = "../home/bookRoom/" . $room["room_id"];
-                }
-                if($room['suite_type'] !== null) {
-                    $suiteType = "Suite Type: ".$room['suite_type'];
-                }else {
-                    $suiteType = '';
-                }
-                echo '<div class="card bg-secondary-clr">
-                <img src="'.BASE_URL.'/public/assets/img/rooms/'.$room["room_image"].'" alt="" class="w-full">
-                <div class="card-text text-white p-3">
-                    <h1 class="wilkysta-font text-3xl">'.$room["room_heading"].'</h1>
-                    <div class="info montserrat-font flex items-center gap-4 text-sm my-2">
-                        <p>Capacity: '.$room["capacity"].'</p>
-                        <i class="bx bxs-circle text-xs text-gray-400"></i>
-                        <p>Room Type: '.$room["room_type"].'</p>
-                        <i class="bx bxs-circle text-xs text-gray-400"></i>
-                        <p>'.$suiteType.'</p>
-                    </div>
-                    <a href="#" class="wilkysta-font uppercase text-lg underline decoration-dotted">see details</a>
-                    <hr class="my-2">
-                    <div class="price-btn wilkysta-font flex items-center justify-between">
-                        <p class="uppercase text-2xl">$<span>'.$room["room_price"].'</span>/night</p>
-                        <a href="'. $bookBtn .'"><button class="border border-2 border-white py-2 px-5 uppercase hover:bg-white hover:text-black transition">book Now</button></a>
-                    </div>
-                </div>
-            </div>';
-            }
+            echo json_encode($rooms);
         }
     }
 }
--- app/view/home/home.php
+++ app/view/home/home.php
@@ -56,7 +56,11 @@
                                 <option value="6">6 Guests</option>
                             </select>
                         </div>
-                        <input type="submit" name="search" id="searchBtn" value="SEARCH" class="uppercase cursor-pointer mt-8 py-2 px-5 bg-black text-white border border-2 border-black wilkysta-font hover:bg-white hover:text-black transition">
+                        <?php if(isset($_SESSION["logged"]) && $_SESSION["logged"] === true):?>
+                            <input type="submit" name="search" id="searchBtn" value="SEARCH" class="uppercase cursor-pointer mt-8 py-2 px-5 bg-black text-white border border-2 border-black wilkysta-font hover:bg-white hover:text-black transition">
+                        <?php else:?>
+                            <a href="../home/log_in" class="uppercase cursor-pointer mt-8 py-2 px-5 bg-black text-white border border-2 border-black wilkysta-font hover:bg-white hover:text-black transition">SEARCH</a>
+                        <?php endif;?>
                     </form>
                 </div>
                 <div class="image mx-7 hidden md:block">

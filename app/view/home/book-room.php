99f8cf7603d50852a35be207ff3927a72418ca06
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-01-15 18:03:15 +0100

    fix book guests form

--- app/view/home/book-room.php
+++ app/view/home/book-room.php
@@ -40,78 +40,20 @@
                 <div class="client-name"><span class="font-medium">Client Name:</span> <?=$_SESSION["username"]?></div>
                 <div class="client-email"><span class="font-medium">Client Email:</span> <?=$_SESSION["email"]?></div>
                 <div class="client-email"><span class="font-medium">Client Phone Number:</span> <?=$_SESSION["phone_number"]?></div>
-            <button class="add-guest hidden py-2 px-4 bg-black text-white hover:bg-transparent hover:text-black hover:border hover:border-2 hover:border-black transition rounded mt-20">Add Guest</button>
+                <div class="book-btns flex items-center gap-3  mt-20">
+                    <button class="add-guest hidden py-2 px-4 bg-black text-white border border-2 border-black hover:bg-transparent hover:text-black hover:border hover:border-2 hover:border-black transition rounded">Add Guest</button>
+                    <form action="../../client/bookRoom/<?=$roomDetails["room_id"]?>" method="post">
+                        <button type="submit" name="try" class="py-2 px-4 bg-black text-white border border-2 border-black rounded hover:bg-transparent hover:text-black hover:border-black transition">Confirm Booking</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
     <!-- form -->
-    <div class="book-form hidden container mx-auto px-96 h-screen">
+    <div class="book-form hidden container mx-auto px-96 h-100 my-12">
         <form method="post" class="h-full form">
             <input type="submit" value="Add Guests" class="py-2 px-4 bg-black text-white rounded">
         </form>
     </div>
-    <div class="container mx-auto px-20">
-        <!-- <a href="../../client/makeReservation"><button class="py-2 px-4 bg-black text-white border border-2 rounded hover:bg-white hover:text-black hover:border-black transition">Confirm Booking</button></a> -->
-        <form action="../../client/bookRoom/<?=$roomDetails["room_id"]?>" method="post">
-            <button type="submit" name="try" class="py-2 px-4 bg-black text-white border border-2 rounded hover:bg-white hover:text-black hover:border-black transition">Confirm Booking</button>
-        </form>
-    </div>
-    <script>
-        let formData = [];
-        let capacity = document.querySelector(".capacity").textContent;
-        let guestsFormDiv = document.querySelector(".book-form");
-        let guestsForm = document.querySelector(".form");
-        let addGuestBtn = document.querySelector(".add-guest");
-        let capacityNum = parseInt(capacity.charAt(9));
-        let guestsAdded = 0;
-        if(capacityNum > 1) {
-            addGuestBtn.classList.remove("hidden");
-        }else {
-            addGuestBtn.classList.add("hidden");
-        }
-            addGuestBtn.addEventListener("click", ()=>{
-                if(guestsAdded === capacityNum-1) {
-                    addGuestBtn.classList.add("hidden");
-                    return;
-                }
-                addGuestForm();
-                guestsAdded++;
-            });
-        function addGuestForm() {
-            let guestForm = `
-            <div>Guest n°: ${guestsAdded+1}</div>
-            <div class="mb-6">
-                <label for="first-name" class="block mb-2 text-sm font-medium text-gray-900">First Name</label>
-                <input type="text" id="first-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="First Name" required>
-            </div>
-            <div class="mb-6">
-                <label for="last-name" class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-                <input type="text" id="last-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Last Name" required>
-            </div>
-            
-            <div class="mb-6">
-                <label for="date-of-birth" class="block mb-2 text-sm font-medium text-gray-900">Birth Date</label>
-                <div class="relative">
-                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                    </div>
-                    <input id="date-of-birth" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Select date">
-                </div>
-            </div>`;
-            guestsFormDiv.classList.remove("hidden");
-            guestsForm.insertAdjacentHTML("afterbegin", guestForm);
-            let guestFirstName = document.querySelector("#first-name");
-            let guestLastName = document.querySelector("#last-name");
-            let guestBirthDate = document.querySelector("#date-of-birth");
-            guestsForm.addEventListener("submit", (e)=>{
-                e.preventDefault();
-                let guestData = {
-                    "firstName": guestFirstName.value,
-                    "lastName": guestLastName.value,
-                    "birthDate": guestBirthDate.value
-                }
-            })
-        }
-    </script>
     <!-- ! end hero -->
     <?php include 'includes/footer.php'?>
--- app/view/home/includes/footer.php
+++ app/view/home/includes/footer.php
@@ -4,6 +4,8 @@
     <script src="https://unpkg.com/flowbite@1.5.5/dist/datepicker.js"></script>
     <!-- jquery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- booking js script -->
+    <script src="<?=BASE_URL?>public/assets/js/book.js"></script>
     <!-- form-validation-script -->
     <script src="<?= BASE_URL?>public/assets/js/form-validation.js"></script>
     <!-- search-form-script -->

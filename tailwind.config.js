85a0d8368f8ab325cbe66c3fd87741a0a56af8c8
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-01-26 19:48:07 +0100

    init node project and install TailwindCSS and add Flowbite plugin

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["app/view/**/*.php", "node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'font-wilkysta': ['Wilkysta'],
+        'font-montserrat': ['Montserrat']
+      },
+      "colors" : {
+        "main-clr" : "#EFEDEA",
+        "secondary-clr" : "#1E1E1E",
+        "accent-clr" : "#DEC2BA"
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
